1ea3a4112e5e59aff285426577d4f47e0c43f9a4
MrDebil <69019135+MrDebil@users.noreply.github.com>
2021-02-12 10:36:10 +0300

    TopDownShooter
    version 1

---
+++ TopDownShooter/Assets/Scripts/LoadMap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMap : MonoBehaviour
+{
+    
+    public GameObject board;
+    public GameObject[] _environments;
+
+    public GameObject _parantEnvironments;
+
+    [SerializeField] private GameObject _tileObject;
+
+    void Start()
+    {
+        LoadBoard();
+    }
+
+    void LoadBoard()
+    {
+        for(var i = -17; i < 17; i++){
+            for(var j = -7; j < 8; j++)
+            {
+
+                var newTile = Instantiate(_tileObject, new Vector3(i, j, 0), Quaternion.identity);
+                newTile.transform.SetParent(board.transform);
+                if((i >= -14 && i <= -9) || (i >= 10 && i <= 14))
+                {
+                    if(j % 2 == 0)
+                    {
+                        if(Random.RandomRange(5, 9) == 6)
+                        {
+                         var _env =  Instantiate(_environments[Random.RandomRange(0, _environments.Length - 1)], new Vector3(i, j, 0), Quaternion.identity);
+                         _env.transform.SetParent(_parantEnvironments.transform);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ TopDownShooter/Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private InputControl _input;
+    private float _speed;
+
+    private Vector3 _deltaMove;
+
+    private Vector3 _endPos;
+    private Vector3 _startPos;
+
+    private Animator _animatorController;
+
+    [SerializeField] private bool _canShoot;
+
+    void Start()
+    {
+        _input = new InputControl();
+        _speed = 3.0f;
+        _deltaMove = new Vector3(0, 0, 0);
+        _startPos = transform.position;
+        _endPos = transform.position;
+        _animatorController = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        _deltaMove = new Vector3(_input.MoveX(), _input.MoveY(), 0) * _speed;
+        _startPos = transform.position;
+        _endPos = transform.position + _deltaMove;
+        transform.position = Vector3.MoveTowards(_startPos, _endPos, Time.deltaTime * _speed * 0.5f);
+
+        
+       
+        ChangePlayerStateAnimation(Mathf.Abs(_input.MoveX()), Mathf.Abs(_input.MoveY()));
+        Flip();
+        if(_canShoot)
+        {
+            if(Input.GetMouseButtonDown(0)){
+
+            }
+        }
+    }
+
+    void ChangePlayerStateAnimation(float _directionX, float _directionY)
+    {
+        if(_directionX > 0 || _directionY > 0)
+        {
+            _animatorController.SetBool("Run", true);
+        }
+        else
+        {
+            _animatorController.SetBool("Run", false);
+        }
+
+    }
+
+    void Flip()
+    {
+        var positionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var angleRotation = Mathf.Atan2(positionMouse.y, positionMouse.x);
+        angleRotation = angleRotation * 180/Mathf.PI;
+        transform.rotation = Quaternion.Euler(0,0, angleRotation);
+
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Gun")
+        {
+            
+        }
+        
+    }
+
+}
