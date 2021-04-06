6d6438bae0a4bf3ce96ac9a2061a64bc7f92a9c8
MrDebil <69019135+MrDebil@users.noreply.github.com>
2021-04-06 23:25:05 +0300

    v1

---
+++ 2Dgame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector3 _normalizedDirection;
+    private float _speed;
+    private float _lifeTime = 5f;
+
+
+    private void Start()
+    {
+        _speed = 10.0f;
+        Destroy(this.gameObject, _lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + _normalizedDirection * _speed, Time.deltaTime * _speed);
+    }
+
+
+
+
+    public void SetDirection(Vector3 direction)
+    {
+        _normalizedDirection = direction.normalized;
+    }
+
+
+}
---
+++ 2Dgame/Assets/Scripts/TridentGun.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TridentGun : Weapon
+{
+    [SerializeField] private GameObject _prebafBullet;
+    [SerializeField] private float Overheat;
+    private const int _numberOfEndsOfTheTrident = 3;
+    private float _lastTime = 0;
+
+    public override void Shoot(Vector3 target, Vector3 arrowPos)
+    {
+        if (CheckRecharge(_lastTime))
+        {
+            var direction = target - arrowPos;
+            var angle = 0.5f;
+            for (var i = 0; i < _numberOfEndsOfTheTrident; i++)
+            {
+
+                CreateBullet(new Vector3(direction.x * Mathf.Cos(angle) - direction.y * Mathf.Sin(angle), direction.y * Mathf.Cos(angle) + direction.x * Mathf.Sin(angle), 0), _prebafBullet);
+                angle -= 0.5f;
+
+            }
+            _lastTime = Time.time + Overheat;
+            StartReload(_lastTime);
+        }
+      
+
+    }
+
+
+
+
+    public override void StartReload(float _lastReload)
+    {
+        base.StartReload(_lastReload);
+    }
+
+
+
+
+
+
+
+
+}
