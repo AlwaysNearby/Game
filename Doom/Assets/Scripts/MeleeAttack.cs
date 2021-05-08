24022afbcb71d4012147b458a3ea1ed2f4dafe58
MrDebil <69019135+MrDebil@users.noreply.github.com>
2021-05-08 17:58:23 +0300

    v1

---
+++ Doom/Assets/Scripts/MeleeAttack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour, IAttackOption
+{
+    public void Charge(float dmg, Collider target)
+    {
+        var decrease = target.GetComponent<IDecrease>();
+        if(decrease != null)
+        {
+            decrease.DecreaseHealth(dmg);
+        }
+    }
+}
